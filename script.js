6511084a15af7ee66752db814b8dac7079252215
cocoa1484 <seddle.PAL@gmail.com>
2025-01-28 09:15:06 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
- <script src="https://www.google.com/recaptcha/api.js"></script>
- <script>
-   function onSubmit(token) {
-     document.getElementById("demo-form").submit();
-   }
- </script>
+<script type="text/javascript">
+  var onloadCallback = function() {
+    alert("grecaptcha is ready!");
+  };
+</script>
+<script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit"
+    async defer>
+</script>
